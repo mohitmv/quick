19405950117dc6e2e72f9c05a7f97b2804d23a83
Mohit Saini <mohit.saini@thoughtspot.com>
2020-03-02 20:16:13 +0530

    Some Change

--- include/quick/type_traits/function_type.hpp
+++ include/quick/type_traits/function_type.hpp
@@ -1,10 +1,21 @@
-// Copyright: 2019 Mohit Saini
+// Copyright: 2020 Mohit Saini
 // Author: Mohit Saini (mohitsaini1196@gmail.com)
+
+// Given a callable type, it creates a `std::function<R(T, U, ..)>` type to
+// hold the callable.
+// Callable type can be any of
+//   1. Function Pointer
+//   2. Member Function Pointer
+//   3. Functor (lambda or otherwise) with exactly one `operator()` member
+//   4. std::function<..>
+// Usage:
+//   look at `tests/type_traits/function_type_test.cpp` for interface details.
 
 #ifndef QUICK_TYPE_TRAITS_FUNCTION_TYPE_HPP_
 #define QUICK_TYPE_TRAITS_FUNCTION_TYPE_HPP_
 
 #include <type_traits>
+#include <functional>
 
 namespace quick {
 
@@ -15,25 +26,21 @@ template<typename...> using void_t = void;
 template <typename T, typename D=void>
 struct function_type_impl {};
 
-// for function pointers
 template <typename ReturnType, typename... Args>
 struct function_type_impl<ReturnType (*)(Args...)>  {
   using type = std::function<ReturnType (Args...)>;
 };
 
-// For generic types that are functors, delegate to its 'operator()'
 template <typename T>
 struct function_type_impl<T, void_t<decltype(&T::operator())>> {
     using type = typename function_type_impl<decltype(&T::operator())>::type;
 };
 
-// for pointers to member function
 template <typename ClassType, typename ReturnType, typename... Args>
 struct function_type_impl<ReturnType(ClassType::*)(Args...) const> {
   using type = std::function<ReturnType (Args...)>;
 };
 
-// for pointers to member function
 template <typename ClassType, typename ReturnType, typename... Args>
 struct function_type_impl<ReturnType(ClassType::*)(Args...) > {
   using type = std::function<ReturnType (Args...)>;
